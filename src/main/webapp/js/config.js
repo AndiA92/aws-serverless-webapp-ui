51afa99203fb807c417b9afdd0e046837dfb60ec
Andrea Aczel <aczelandi@gmail.com>
2017-10-20 07:55:10 +0300

    Add index.html + js indcludes

---
+++ src/main/webapp/js/config.js
@@ -0,0 +1,10 @@
+window._config = {
+    cognito: {
+        userPoolId: 'eu-central-1_veLegRRNO',
+        userPoolClientId: '3c414g3pjn9rcaa97mnrh25s8t',
+        region: 'eu-central-1'
+    },
+    api: {
+        invokeUrl: 'https://mrnisigrqe.execute-api.eu-central-1.amazonaws.com/TEST'
+    }
+};
